7a40119bc4d3f22d26f325758ec2720c63efabdd
MarynaHolub <maryna.holub27@gmail.com>
2026-09-23 08:38:01 +0200

    Node.js HW1

---
+++ simpleLogger/app.js
@@ -0,0 +1,7 @@
+const logger = require ('./logger')
+
+const text1 = 'Hello Maryna!'
+const text2 = 'How are you?'
+
+logger(text1)
+logger(text2)
---
+++ simpleLogger/logger.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+function logMessage(text){
+    fs.appendFile('log.txt', text + '\n', (err)=>{
+        if (err){
+            console.error(err);
+        }
+    } )
+}
+
+module.exports = logMessage;
