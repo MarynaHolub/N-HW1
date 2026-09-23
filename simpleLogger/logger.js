5d44ab1297c90b26600e7564dd008847f00b855c
MarynaHolub <maryna.holub27@gmail.com>
2026-09-23 16:41:43 +0200

    fixed bug

--- simpleLogger/logger.js
+++ simpleLogger/logger.js
@@ -1,11 +1,24 @@
-const fs = require('fs')
+const fs = require("fs").promises;
 
-function logMessage(text){
-    fs.appendFile('log.txt', text + '\n', (err)=>{
-        if (err){
-            console.error(err);
-        }
-    } )
+async function logMessage(message) {
+  try {
+    await fs.appendFile("log.txt", message + "\n");
+    console.log("Сообщение записано в лог");
+  } catch (err) {
+    console.error("Ошибка при записи лога:", err);
+  }
 }
 
-module.exports = logMessage;
+module.exports = { logMessage };
+
+// const fs = require('fs')
+
+// function logMessage(text){
+//     fs.appendFile('log.txt', text + '\n', (err)=>{
+//         if (err){
+//             console.error(err);
+//         }
+//     } )
+// }
+
+// module.exports = logMessage;
